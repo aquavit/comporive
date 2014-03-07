d4891a470a2c5d0558ad3c3a155f5692c1d2dd56
tMaro <maro.error1989@gmail.com>
2014-03-08 01:16:51 +0900

    //Drummer更新(BPMでドラムの早さが変わるように変更しました)

--- src/jp/comporive/Drummer.java
+++ src/jp/comporive/Drummer.java
@@ -2,6 +2,7 @@ package jp.comporive;
 
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.util.Log;
 
 /**ドラマー*/
 public class Drummer {
@@ -13,18 +14,35 @@ public class Drummer {
 	double nowBpm = 0.0;	//現在のbpm
 	double targetBpm = 0.0;	//ターゲットとなるbpm
 	
+	//private static int BUF_SIZE = 30;
+	
 	//ドラムデータ
 	private SoundPool sound = null;
 	private int dMax = 10;	//同時利用数
 	private int id;	//ID
-	private double wait = 0;	//ドラムを鳴らすポイント
-
+	private long wait = 0;	//ドラムを鳴らすポイント
+	private long target = 0;	
+	//private long list[] = new long[BUF_SIZE];
+	//private int focus = 0;
+	
+	
 	/**初期化*/
 	public void initialize(){
 		//音情報を保存する
 		sound = new SoundPool(dMax, AudioManager.STREAM_MUSIC, 0);
-	//	id = sound.load(AppliData.context, R.raw.a0, 1);
+		id = sound.load(AppliData.context, R.raw.a0, 1);
 		
+		//
+		long now = System.currentTimeMillis();
+		target =  1000;
+		samMill = 0;
+		
+		/*
+		//鳴らすタイミングを計算する
+		for (int i=0; i<BUF_SIZE; i++){
+			list[i] = now+1000 + (1000*i);
+		}
+		*/
 		//
 		//calcBpm();
 	}
@@ -36,6 +54,11 @@ public class Drummer {
 	
 	/**処理*/
 	public void process(){
+		//calcBpm();
+		
+		//時間を算出する
+		//calcTime();
+		
 		//bpm数を計算する
 		playSound();
 	}
@@ -43,36 +66,40 @@ public class Drummer {
 	int cnt = 0;
 
 	/**音を鳴らす*/
-	public void playSound(){
+	private void playSound(){
 		//BPMを算出する
-		//calcBpm();
+		calcBpm();
 		
 		//時間を算出する
-		//calcTime();
+		calcTime();
 		
 		//bpmを鳴らす
-		//playDrum();
+		playDrum();
+		
+		/*
 		if (++cnt >= 60){
-			sound.play(id, 1.0F, 1.0F, 1, 0, 1.0F);
+		
 			cnt = 0;
 		}
+		*/
 	}
-
-	
 	
 	/**ドラムを鳴らす*/
-	public void playDrum(){
-		long target1 = 0;	
+	private void playDrum(){
+		Log.e("Time", "target = "+target);
+		Log.e("Time", "sam = "+samMill);
 		
 		//ターゲットまでの音が出ていない
-		if (target1 < addMill)
+		if (target > samMill)
 			return;
+
+		//
+		target = samMill + wait;
 		
 		//音を鳴らす
-		
-		//ターゲットを更新する
-		
-		
+		sound.play(id, 1.0F, 1.0F, 1, 0, 1.0F);
+
+		Log.e("Sound", "PLAY SOUND--------------------");
 	}
 	
 	/**bpmを渡す*/
@@ -86,20 +113,26 @@ public class Drummer {
 	private void calcBpm(){
 		
 		//仮代入
-		nowBpm = 60.0;
+		nowBpm = 180.0;
 		
 		//ドラムを鳴らすミリ秒を計算する
 		if (nowBpm <= 0.0){
-			wait = 0.0;
+			wait = 0;
 		}else{
-			//1秒間に鳴らすタイミングを設定する
-			wait = 1000 / nowBpm;
+			//60BPM は 1000ミリ(1秒)に一回なので、割合を算出する
+			double rate = 60.0 / nowBpm;
+			wait = (long)(1000.0 * rate);
 		}
-			
 	}
 	
 	/**時間計測*/
 	private void calcTime(){
+		//初期時の処理
+		if (oldMill == -1){
+			oldMill = System.currentTimeMillis();
+			return;
+		}
+			
 		//現在の時間を取得する
 		long nowMill = System.currentTimeMillis();
 
@@ -107,11 +140,13 @@ public class Drummer {
 		long sub = nowMill - oldMill;
 		
 		//プラスする
-		addMill += sub;
+		samMill += sub;
 		
+		//
+		oldMill = nowMill;
 	}
 
-	private long oldMill = 0; //昔のミリサイズ
-	private long addMill = 0;	//総ミリ
+	private long oldMill = -1; //昔のミリサイズ
+	private long samMill = 0;	//総ミリ
 
 }
--- src/jp/comporive/SoundThread.java
+++ src/jp/comporive/SoundThread.java
@@ -1,7 +1,21 @@
 package jp.comporive;
 
 class SoundThread extends Thread{
+	public boolean shutdown = false;
+	Drummer drum = new Drummer();
+	
 	@Override
 	public void run() {
+	
+		//ドラムの初期化
+		drum.initialize();
+		
+		while (!shutdown){
+			//ドラム処理
+			drum.process();
+		}
+		
+		//ドラムの終了処理
+		drum.shutdown();
 	}
 }
