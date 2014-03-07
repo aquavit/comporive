607df132e1ff10c619d3c4737e6cca764c64773d
sasaki_yutaka <sasaki_yutaka@trek.co.jp>
2014-03-07 21:50:03 +0900

    空のandroidプロジェクト

---
+++ src/jp/comporive/MyActivity.java
@@ -0,0 +1,29 @@
+package jp.comporive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SensorThread sensor = new SensorThread();
+        SoundThread sound = new SoundThread();
+
+        sensor.start();
+        sound.start();
+
+        setContentView(R.layout.main);
+    }
+}
+
+class SensorThread extends Thread{
+
+}
+
+class SoundThread extends Thread{
+
+}
