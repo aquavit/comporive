cec4957c8748a0bf333cb10fbe1dde466703a354
ntabee <tabuchi_naoshi@trek.co.jp>
2014-03-08 01:17:35 +0900

    速度計算 & queue に入れる所まで。 (* VelocityIntegrator -> VelocityAccumulator に変更)

--- src/jp/comporive/MyActivity.java
+++ src/jp/comporive/MyActivity.java
@@ -1,7 +1,12 @@
 package jp.comporive;
 
+import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -17,8 +22,9 @@ public class MyActivity extends Activity {
 
         SensorManager sensorManager = (SensorManager)getSystemService( SENSOR_SERVICE  );
 
-        SensorThread sensor = new SensorThread(sensorManager);
-        SoundThread sound = new SoundThread();
+        BlockingQueue<Velocity> queue = new ArrayBlockingQueue<Velocity>(70 * 60); // 70FPS を仮定して、1分間分 (とりあえず)
+        SensorThread sensor = new SensorThread(sensorManager, queue);
+        SoundThread sound = new SoundThread(queue);
 
         sensor.start();
         sound.start();
@@ -32,19 +38,62 @@ class SensorThread extends Thread implements SensorEventListener{
     SensorManager sensorManager;
     Sensor sensor;
 
-    public SensorThread(SensorManager sensorManager){
+    private final BlockingQueue<Velocity> queue;
+    private VelocityAccumlator velocity = new VelocityAccumlator(0.0, 0.0, 0.0); // 速度ベクトルの初期値は (0, 0, 0) を仮定
+    public SensorThread(SensorManager sensorManager, BlockingQueue<Velocity> queue){
         this.sensorManager = sensorManager;
+        this.queue = queue;
     }
     @Override
     public void run(){
-        sensor = sensorManager.getDefaultSensor( Sensor.TYPE_ACCELEROMETER );
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+    	final int RATE = 16670;	// 16.67 msec in micro seconds
+    	// TYPE_ACCELEROMETER -> 出力に「重力」が含まれる (机に置きっぱなしでも 9.8 前後の値が出る)
+    	// Sensor.TYPE_LINEAR_ACCELERATION -> 重力を除いた補正値が出る (Androidバージョンによって使えなかったり値がいいかげんらしい?)
+        sensor = sensorManager.getDefaultSensor( Sensor.TYPE_LINEAR_ACCELERATION /*Sensor.TYPE_ACCELEROMETER*/ );
+        sensorManager.registerListener(this, sensor, RATE /*SensorManager.SENSOR_DELAY_UI*/);
     }
 
     public void onAccuracyChanged (Sensor sensor, int accuracy) {
 
     }
+    
+    private long prevTimestamp = 0L;
+    final double NANOSEC_SCALE = 1000 * 1000 * 1000;
+    private long count = 0L;
+    
+    private float initialValues[];
     public void onSensorChanged(SensorEvent sensorEvent) {
-
+    	long deltaT = sensorEvent.timestamp - prevTimestamp;
+    	prevTimestamp = sensorEvent.timestamp;
+    	
+    	if (count == 0) {
+    		// 最初のデータは、timestamp と加速度の値だけ覚えておいて読みとばす
+    		initialValues = Arrays.copyOf(sensorEvent.values, sensorEvent.values.length);
+        	count++;
+    		return;
+    	}
+    	double deltaTinSec = deltaT / NANOSEC_SCALE;
+    	
+    	float values[] = Arrays.copyOf(sensorEvent.values, sensorEvent.values.length);
+    	// initialValues でキャリブレート
+    	for (int i=0; i<values.length; i++) {
+    		values[i] -= initialValues[i];
+    	}
+    	velocity.update(values[0], values[1], values[2], deltaTinSec);
+    	
+    	try {
+			queue.put(velocity.current());
+			Log.d("comporive", velocity.current().toString());
+		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+		}
+//    	Log.d("comporive", 
+//    		sensorEvent.timestamp + " (delta = " + deltaTinSec + " sec.): " + 
+//    		Arrays.toString(values) + 
+//    		" velocity = " + velocity.magnitude() +
+//    		"(" + velocity.velocityX() + ", " + velocity.velocityY() + ", " + velocity.velocityZ() + ")");
+    	
+    	count++;
     }
 }
--- src/jp/comporive/SoundThread.java
+++ src/jp/comporive/SoundThread.java
@@ -1,6 +1,12 @@
 package jp.comporive;
 
+import java.util.concurrent.BlockingQueue;
+
 class SoundThread extends Thread{
+	private final BlockingQueue<Velocity> queue;
+	public SoundThread(BlockingQueue<Velocity> queue) {
+		this.queue = queue;
+	}
 	@Override
 	public void run() {
 	}
